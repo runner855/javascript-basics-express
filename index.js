3ede2276891581b4beb55c29546366b09f6c80d9
Dario Milani <runner855@icloud.com>
2020-12-13 16:41:01 +0100

    multiply nearly finishe

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = require('./src/app');
 const APP_PORT = 3000;
 
 app.listen(APP_PORT, () => {
-  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); // eslint-disable-line
+  // eslint-disable-next-line no-console
+  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { add, subtract } = require('./lib/numbers');
+
+const { add, subtract, multiply } = require('./lib/numbers');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/strings/hello/:turtle', (req, res) => {
   res.json({ result: `Hello, ${req.params.turtle}!` });
@@ -42,6 +45,20 @@ app.get('/numbers/subtract/:b/from/:a', (req, res) => {
   }
 
   res.status(200).json({ result: subtract(b, a) });
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  const a = Number(req.body.b);
+  const b = Number(req.body.a);
+
+  if (!a && !b) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (Number.isNaN(a, b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+
+  res.json({ result: multiply(a, b) });
 });
 
 module.exports = app;
